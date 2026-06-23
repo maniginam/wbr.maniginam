fc6db2ecd236170107fe858e3b928a61bf1696bc
Gina <maniginam@gmail.com>
2026-06-23 16:01:19 -0500

    feat(content): markdown to article record

--- app/src/wbr/content.clj
+++ app/src/wbr/content.clj
@@ -1,9 +1,19 @@
 (ns wbr.content
   (:require [clojure.edn :as edn]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [markdown.core :as md]))
 
 (defn parse-frontmatter [s]
   (if-let [[_ meta-str body] (re-matches #"(?s)---\n(.*?)\n---\n?(.*)" s)]
     {:meta (edn/read-string meta-str)
      :body (str/triml body)}
     {:meta {} :body s}))
+
+(defn ->article [slug raw]
+  (let [{:keys [meta body]} (parse-frontmatter raw)]
+    {:slug slug
+     :title (or (:title meta) slug)
+     :category (:category meta)
+     :summary (:summary meta)
+     :updated (:updated meta)
+     :html (md/md-to-html-string body)}))
--- app/test/wbr/content_test.clj
+++ app/test/wbr/content_test.clj
@@ -9,3 +9,13 @@
 (deftest parse-frontmatter-without-frontmatter
   (is (= {:meta {} :body "Just text"}
          (c/parse-frontmatter "Just text"))))
+
+(deftest ->article-builds-record
+  (let [a (c/->article "best-coffee" "---\n{:title \"Coffee\" :category \"Food\"}\n---\n# Hi")]
+    (is (= "best-coffee" (:slug a)))
+    (is (= "Coffee" (:title a)))
+    (is (= "Food" (:category a)))
+    (is (re-find #"<h1>Hi</h1>" (:html a)))))
+
+(deftest ->article-title-falls-back-to-slug
+  (is (= "my-slug" (:title (c/->article "my-slug" "no frontmatter body")))))
